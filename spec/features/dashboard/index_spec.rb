6d8ffc76c4b4604c7a786f663fac59494822be72
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-08-23 22:29:50 -0700

    Test: add test for search bar on dashboard

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -2,10 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Dashboard/Welcome Page' do
     describe "Dashboard Contents" do
-        it 'has a title' do
+        before :each do
             visit root_path
+        end
+
+        it 'has a title' do
             expect(page).to have_content("Satoshi Explorer")
             expect(page).to have_content("Search Blockchain")
+        end
+
+        it 'has a search field' do
+            fill_in :search, with: "000000000000000015dc777b3ff2611091336355d3f0ee9766a2cf3be8e4b1ce"
+            click_button "Search"
+            expect(current_path).to eq(search_path)
         end
     end
 end
