5c13e01cf91d83bb5af0a132a6902fdca6fe6081
Antonio Salmeron <mr.tonyboi@gmail.com>
2023-03-17 19:43:30 +0100

    Add: mempool dashboard facade

--- app/facades/dashboard_facade.rb
+++ app/facades/dashboard_facade.rb
@@ -1,7 +1,15 @@
 class DashboardFacade
     
     def self.block_tip_height
-        response = DashboardService.get_block_tip_height
+        json = DashboardService.get_block_tip_height
+    end
+
+    def self.all_mempool
+        json = DashboardService.get_all_mempool
+    end
+
+    def self.recent_mempool
+        json = DashboardService.get_mempool_recent
     end
     
 end
