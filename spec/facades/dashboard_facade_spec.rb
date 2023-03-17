fe91be071f2474f6faedc9b4155e1999f8518318
Antonio Salmeron <mr.tonyboi@gmail.com>
2023-03-17 19:42:58 +0100

    Test: dashboard facade for mempool stats

--- spec/facades/dashboard_facade_spec.rb
+++ spec/facades/dashboard_facade_spec.rb
@@ -8,5 +8,18 @@ RSpec.describe DashboardFacade do
       expect(height).to be_a(Integer)
     end
 
+    xit 'returns mempool stats' do
+      mempool_stat = DashboardFacade.all_mempool
+      expect(mempool_stat[:txid]).to be_a(String)
+      expect(mempool_stat[:count]).to be_a(Integer)
+      expect(mempool_stat[:vsize]).to be_a(Integer)
+      expect(mempool_stat[:total_fee]).to be_a(Integer)
+    end
+
+    xit 'returns latest 10 tx in mempool' do
+      mempool_recent = DashboardFacade.mempool_recent
+      expect(mempool_recent.count).to be_a(10)
+    end
+
   end
 end
