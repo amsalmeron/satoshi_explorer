2503959e25d19799c8b75b1236bbff599f31beba
Antonio Salmeron <mr.tonyboi@gmail.com>
2023-03-19 20:19:14 +0100

    Update: lightning and mining controller index pages

---
+++ app/controllers/lightning_controller.rb
@@ -0,0 +1,7 @@
+class LightningController < ApplicationController
+    
+    def index
+        
+    end
+
+end
--- app/controllers/mining_controller.rb
+++ app/controllers/mining_controller.rb
@@ -1,7 +1,7 @@
 class MiningController < ApplicationController
     
     def index
-        
+        @mining_pools = MiningFacade.mining_pools
     end
 
 end
