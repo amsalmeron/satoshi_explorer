c15db11a0e5756f9fc060f681aee7bdf86c2594e
Antonio Salmeron <mr.tonyboi@gmail.com>
2023-03-19 20:18:26 +0100

    Update: lighning path route for navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   get '/explorer', to: 'explorer#index'
   get '/mining', to: 'mining#index'
-  get '/network', to: 'network#index'
+  get '/lightning', to: 'lightning#index'
   get '/resources', to: 'resources#index'
   
   get '/search', to: 'search#index'
